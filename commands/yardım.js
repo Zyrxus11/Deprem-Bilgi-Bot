ea3282ff90e37cc3f8c784ef9016576f4b9b97f8
Zyrxus <74361674+Zyrxus11@users.noreply.github.com>
2022-10-01 22:33:51 +0300

    Deprem Bilgi v2

--- commands/yardım.js
+++ commands/yardım.js
@@ -1,17 +1,27 @@
 const Discord = require("discord.js"),
 client = new Discord.Client();
-
+const db = require("quick.db")
 module.exports.run = async (client, message, args) => {
     const embed = new Discord.MessageEmbed()
     .setTitle('Deprem Bilgi | Yardım Menüsü')
     .setThumbnail(client.user.avatarURL())
+    .setColor("BLACK")
+    .setFooter('Copyright © Executable Studio')
     .setAuthor('Deprem Bilgi', client.user.avatarURL())
     .setDescription(`
+> Son Deprem Nerde Oldu?
+> Lokasyon: **${db.fetch(`sondeprem2`) || 'Son deprem bilgisi alınamadı'} **
 
-\`d!yardım\` **=** Deprem Bilgi botunun yardım menüsünü gösterir.
-\`d!deprem\` **=** Deprem Bilgi ayarlarını yaparsınız.
-\`d!istatistik\` **=** Botun istatistiklerini gösterir.
-\`d!ping\` **=** Gecikme süresini gösterir.
+**d!yardım** \`=\` Yardım menüsünü gösterir.
+**d!deprem** \`=\` Deprem Bilgi sistemi komutları.
+**d!istatistik** \`=\` Bot istatistigi.
+**d!ping** \`=\` Botun gecikme süresini gösterir.
+
+Linkler \`=>\`
+[Destek Sunucusu](https://discord.gg/EZ673nyaNj)
+[Destekle (Oy ver)](https://top.gg/bot/1000057864980811878/votes)
+[Destekle (Botu ekle)](https://discord.com/api/oauth2/authorize?client_id=1000057864980811878&permissions=8&scope=bot)
+
 `)
     
 message.channel.send(embed)
